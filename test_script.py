c21823e81a77c47952c715ab718bbdef441186bd
Muhammad Inaamullah <94857175+m-inaam@users.noreply.github.com>
2023-11-19 05:24:50 +0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,53 +1,59 @@
 import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
+from deepface import DeepFace
 
-cv2.ocl.setUseOpenCL(False)
+# Load the pre-trained emotion detection model
+model = DeepFace.build_model("Emotion")
 
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+# Define emotion labels
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-# Load the cascade classifier
+# Load face cascade classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Load the emotion classification model (assuming 'emotion_model' is already defined)
-# Add proper error handling in case the model loading fails
-try:
-    # Load your emotion classification model here
-    emotion_model = load_model('model.h5')  # Update with the actual path
-except Exception as e:
-    print(f"Error loading the emotion model: {e}")
-    exit()
-
-def detect_emotion(frame):
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    num_faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
-
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-    return frame
-
-# Open a video capture object
+# Start capturing video
 cap = cv2.VideoCapture(0)
 
 while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if not ret:
-        break
 
-    frame_with_emotion = detect_emotion(frame)
+    # Convert frame to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Video', cv2.resize(frame_with_emotion, (1200, 860), interpolation=cv2.INTER_CUBIC))
-    
-    # Wait for the 'q' key press to exit the loop
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # Extract the face ROI (Region of Interest)
+        face_roi = gray_frame[y:y + h, x:x + w]
+
+        # Resize the face ROI to match the input shape of the model
+        resized_face = cv2.resize(face_roi, (48, 48), interpolation=cv2.INTER_AREA)
+
+        # Normalize the resized face image
+        normalized_face = resized_face / 255.0
+
+        # Reshape the image to match the input shape of the model
+        reshaped_face = normalized_face.reshape(1, 48, 48, 1)
+
+        # Predict emotions using the pre-trained model
+        preds = model.predict(reshaped_face)[0]
+        emotion_idx = preds.argmax()
+        emotion = emotion_labels[emotion_idx]
+
+        # Draw rectangle around face and label with predicted emotion
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Real-time Emotion Detection', frame)
+
+    # Press 'q' to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close windows
+# Release the capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
+
+
